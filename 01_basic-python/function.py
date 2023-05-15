7fc8fd001c21591c3ea11e4b3844cd536eed0cad
ToroonEX <michigiek@t-online.de>
2023-05-15 12:53:15 +0200

    changed

---
+++ 01_basic-python/function.py
@@ -0,0 +1,15 @@
+import math
+
+
+def distance(x, y):
+    a=[0,0,0]
+    for i in range(3):
+        a[i] = (x[i]-y[i])**2
+    lsg=math.sqrt(a[0]+a[1]+a[2])
+    return lsg
+
+
+x = [2, 3, 1]
+y = [4, 1, -2]
+
+print(distance(x, y))
